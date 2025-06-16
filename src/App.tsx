2a5c80c2614b14eff11a738bdf23fa86ea8a9b74
Ihor Havrylov <i-havr@MacBook-Air-Ihor.local>
2025-06-16 12:12:02 +0300

    feat(tg): add tg web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import useExpand from './hooks/useExpand'
 
 function App() {
   const [count, setCount] = useState(0)
+  const {handleExpand, isExpanded} = useExpand();
+
+  useEffect(() => {
+    if (!isExpanded) handleExpand();
+  }, [handleExpand, isExpanded]);
 
   return (
     <>
