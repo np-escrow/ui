f966e4791402c7bc398715a690dd505c4cc3199c
Nikita Sokolov <nsokolov0309@gmail.com>
2025-06-17 18:14:59 +0300

    feat(root): add locale pull dev

---
+++ src/components/DeliveriesListItem/DeliveriesListItem.tsx
@@ -0,0 +1,81 @@
+import { useMemo, type FC } from "react";
+import { EDeliveryStatus, EUserType, type IDeliveries } from "../../types";
+import classNames from "classnames";
+import styles from "./DeliveriesListItem.module.css";
+import { t } from "i18next";
+
+import DeliveriesItemInfo from "../DeliveriesItemInfo/DeliveriesItemInfo";
+
+interface DeliveriesListItemProps {
+  item: IDeliveries;
+}
+
+const DeliveriesListItem: FC<DeliveriesListItemProps> = ({ item }) => {
+  const subtitle = useMemo(() => {
+    if (item.status === EDeliveryStatus.PENDING) {
+      return item.userType === EUserType.SELLER
+        ? t("deliveries.item.sub-to", {
+            recipient: `${item.info.recipient}, ${item.info.recipientCity}`
+          }) //
+        : t("deliveries.item.sub-from", {
+            seller: `${item.info.seller}, ${item.info.sellerCity}`
+          }); // ;
+    }
+    return item.userType === EUserType.SELLER
+      ? t("deliveries.item.sub-seller")
+      : t("deliveries.item.sub-recepient");
+  }, [item]);
+
+  const status = useMemo(() => {
+    if (item.status === EDeliveryStatus.PENDING) {
+      if (item.userType === EUserType.RECIPIENT) {
+        return t("deliveries.item.statuspending-recepient");
+      } else {
+        return t("deliveries.item.statuspending-seller");
+      }
+    }
+    if (item.status === EDeliveryStatus.PAID) {
+      return t("deliveries.item.status-paid");
+    }
+    if (item.status === EDeliveryStatus.COMPLETED) {
+      return item.archive
+        ? t("deliveries.item.status-completed-archive")
+        : t("deliveries.item.status-completed");
+    }
+    return item.status;
+  }, [item.status, item.userType, item.archive]);
+
+  return (
+    <div
+      className={classNames(styles.deliveries__box, {
+        [styles.completed]:
+          item.status === EDeliveryStatus.COMPLETED && item.archive
+      })}
+    >
+      <div className="w-full">
+        <div className="flex items-center justify-between">
+          <p className={styles.deliveries__header}>{item.ttn}</p>
+          <p
+            className={styles.deliveries__header}
+          >{`${item.price} ${item.currency}`}</p>
+        </div>
+        <p className={styles.deliveries__subtitle}>{subtitle}</p>
+      </div>
+      {!item.archive && item.status !== EDeliveryStatus.PENDING && (
+        <DeliveriesItemInfo
+          isDeliveried={new Date(item.info.deliveryDate).getTime() < Date.now()}
+          createdAt={item.info.createdAt}
+          deliveryDate={item.info.deliveryDate}
+          sellerCity={item.info.sellerCity}
+          recepientCity={item.info.recipientCity}
+        />
+      )}
+      <div className="h-[1px] w-full bg-[#bcc3d080]" />
+      <p className={styles.deliveries__status}>
+        {t("deliveries.item.status")}: <span>{status}</span>
+      </p>
+    </div>
+  );
+};
+
+export default DeliveriesListItem;
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,22 +1,20 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import en from "./locales/en.json";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import ru from "./locales/ru.json";
+import uk from "./locales/uk.json";
 
-import uk from './locales/uk.json';
-import ru from './locales/ru.json';
-import en from './locales/en.json';
+i18n.use(initReactI18next).init({
+  resources: {
+    uk: { translation: uk },
+    ru: { translation: ru },
+    en: { translation: en }
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false
+  }
+});
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      uk: { translation: uk },
-      ru: { translation: ru },
-      en: { translation: en }
-    },
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false
-    }
-  });
-
-export default i18n; 
+export default i18n;
